aa6b4cce8e01a550a149488c22584b3166cdea39
Joona21010 <91433596+Joona21010@users.noreply.github.com>
2021-11-25 19:53:54 +0200

    1.luokka

--- src/LeluLuokka.java
+++ src/LeluLuokka.java
@@ -3,7 +3,30 @@ public class LeluLuokka {
 
 	public static void main(String[] args) {
 		
-		System.out.println("2");
+		
 	}
 
+}
+
+class Lelu
+{
+	String name;
+	int weight;
+	
+	public Lelu(String n, int w)
+	{
+		name = n;
+		weight = w;
+	}
+	
+	public void liiku()
+	{
+		System.out.println("Lelu liikkuu");
+	}
+	
+	public void tulostaTiedot()
+	{
+		System.out.println("Nimi: " + name);
+		System.out.println("Paino " + weight);
+	}
 }
